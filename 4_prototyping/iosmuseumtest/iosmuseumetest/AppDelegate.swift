f1b706f391cb352b15e176b317fc62e2c7f8d585
Jonathan Tang <jtang002@gold.ac.uk>
2018-11-15 11:53:31 +0000

    adding ios prototype

---
+++ 4_prototyping/iosmuseumtest/iosmuseumetest/AppDelegate.swift
@@ -0,0 +1,21 @@
+/*
+AppDelegate.swift
+iosmuseumetest
+ 
+Created by Jonathan Tang on 12/11/2018.
+ 
+Completed with Medium tutorial
+https://medium.com/codeandco/building-an-iphone-ar-museum-app-in-ios-11-with-apples-arkit-image-recognition-b07febd90a91
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
---
+++ 4_prototyping/iosmuseumtest/iosmuseumetest/Scene.swift
@@ -0,0 +1,14 @@
+/*
+Scene.swift
+iosmuseumetest
+ 
+Created by Jonathan Tang on 12/11/2018.
+ 
+Completed with Medium tutorial
+https://medium.com/codeandco/building-an-iphone-ar-museum-app-in-ios-11-with-apples-arkit-image-recognition-b07febd90a91
+*/
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {}
