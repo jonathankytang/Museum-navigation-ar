af322813f787d6f5094d8b63dfc53df715b99849
Jonathan Tang <jtang002@gold.ac.uk>
2018-11-23 20:07:46 +0000

    reorganise directory and android sensors

--- 4_prototyping/backend/AndroidSensors/app/src/main/java/sp14/androidsensors/MainActivity.java
+++ 4_prototyping/backend/AndroidSensors/app/src/main/java/sp14/androidsensors/MainActivity.java
@@ -1,53 +1,7 @@
 package sp14.androidsensors;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.Scanner;
-
-//public class MainActivity extends AppCompatActivity implements SensorEventListener {
-//
-//    private static final String TAG = "MainActivity";
-//
-//    private SensorManager sensorManager;
-//    Sensor accelerometer;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        Log.d(TAG, "onCreate: Initializing Sensor Services");
-//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-//
-//        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-//        Log.d(TAG, "onCreate: Registered accelerometer listener");
-//    }
-//
-//    @Override
-//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//
-//    }
-//
-//    @Override
-//    public void onSensorChanged(SensorEvent event) {
-//        Log.d(TAG, "onSensorChanged: X: " + event.values[0] + " Y :" + event.values[1] + " Z: " + event.values[2]);
-//    }
-//}
-
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
@@ -137,17 +91,19 @@ public class MainActivity extends Activity implements SensorEventListener{
         Sensor sensor = event.sensor;
 
         if(sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            Log.d(TAG, "onSensorChanged: X: " + event.values[0] + " Y :" + event.values[1] + " Z: " + event.values[2]);
+            Log.d(TAG, "Accelerometer: X: " + event.values[0] + " Y :" + event.values[1] + " Z: " + event.values[2]);
             xAccel.setText("X: " + event.values[0]);
             yAccel.setText("Y: " + event.values[1]);
             zAccel.setText("Z: " + event.values[2]);
         }
         else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            Log.d(TAG, "Gyroscope: X: " + event.values[0] + " Y :" + event.values[1] + " Z: " + event.values[2]);
             xGyro.setText("X: " + event.values[0]);
             yGyro.setText("Y: " + event.values[1]);
             zGyro.setText("Z: " + event.values[2]);
         }
         else if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            Log.d(TAG, "Magnetometer: X: " + event.values[0] + " Y :" + event.values[1] + " Z: " + event.values[2]);
             xMagno.setText("X: " + event.values[0]);
             yMagno.setText("Y: " + event.values[1]);
             zMagno.setText("Z: " + event.values[2]);
